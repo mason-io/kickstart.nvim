03bd4f300c544e4f1db7df2119a025524b93a120
Mason McEwen <62111700+mason-io@users.noreply.github.com>
2025-10-01 14:27:29 -0600

    chore: add shared Vale config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,9 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+local vale_config = vim.fn.stdpath('config') .. '/vale/.vale.ini'
+
+if vim.fn.filereadable(vale_config) == 1 then
+  vim.env.VALE_CONFIG_PATH = vale_config
+end
+
 return {}
